6de4f6edd0f4d24b47eff32579e8c7bc46b53fc6
RicoEDLI <Ricomeschede@gmail.com>
2017-12-03 13:40:07 +0100

    added Githublink

--- @BWSnowCamo/mod.cpp
+++ @BWSnowCamo/mod.cpp
@@ -1,6 +1,8 @@
 
 name = "Bundeswehr Schneetarn";
 picture = "bw_snow_logo.paa";
+actionName = "Github";
+action = "https://github.com/RicoEDLI/BWSnowCamo";
 description = "Bundeswehr Schneetarn";
 logo = "bw_snow_logo.paa";
 logoOver = "bw_snow_logo.paa";
