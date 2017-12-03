998b7b4797f1b865b8fbe26be362f1657f9efac9
RicoEDLI <Ricomeschede@gmail.com>
2017-12-03 02:11:12 +0100

    added tools

---
+++ tools/pack.bat
@@ -0,0 +1,6 @@
+
+"F:\Steam\steamapps\common\Arma 3 Tools\AddonBuilder\AddonBuilder.exe"^
+    "H:\Arma 3 Arbeitsverzeichnis\Entwicklung\BWSnowCamo\@BWSnowCamo\addons\bw_snowCamo"^
+    "H:\Arma 3 Arbeitsverzeichnis\Entwicklung\BWSnowCamo\.build\@BW_Schneetarn\addons"^
+    "-prefix=bw_snowCamo"^
+    "-packonly"
