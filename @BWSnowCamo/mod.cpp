cead142c66e174c0b407d132c48f6064a1918593
RicoEDLI <Ricomeschede@gmail.com>
2017-12-03 13:08:06 +0100

    VIncent als Author hinzugefügt

--- @BWSnowCamo/mod.cpp
+++ @BWSnowCamo/mod.cpp
@@ -4,5 +4,5 @@ picture = "bw_snow_logo.paa";
 description = "Bundeswehr Schneetarn";
 logo = "bw_snow_logo.paa";
 logoOver = "bw_snow_logo.paa";
-author = "RicoEDLI";
+author = "RicoEDLI & TheMysteriousVincent";
 overviewPicture = "bw_snow_logo.paa";
