b6d42ec2b9c5f23ac669c5af94ec131376a96d07
RicoEDLI <Ricomeschede@gmail.com>
2017-12-09 14:31:22 +0100

    pack bat change

--- tools/pack.bat
+++ tools/pack.bat
@@ -1,6 +1,9 @@
 
 "F:\Steam\steamapps\common\Arma 3 Tools\AddonBuilder\AddonBuilder.exe"^
-    "H:\Arma 3 Arbeitsverzeichnis\Entwicklung\BWSnowCamo\@BWSnowCamo\addons\bw_snowCamo"^
+    "H:\Arma 3 Arbeitsverzeichnis\Entwicklung\BWSnowCamo\@BWSnowCamo\addons\bwsh_h_m92"
+    "H:\Arma 3 Arbeitsverzeichnis\Entwicklung\BWSnowCamo\@BWSnowCamo\addons\bwsc_main"
+    "H:\Arma 3 Arbeitsverzeichnis\Entwicklung\BWSnowCamo\@BWSnowCamo\addons\bwsh_u_b_combatuniform"
+    "H:\Arma 3 Arbeitsverzeichnis\Entwicklung\BWSnowCamo\@BWSnowCamo\addons\bwsh_u_i_combatuniform"^
     "H:\Arma 3 Arbeitsverzeichnis\Entwicklung\BWSnowCamo\.build\@BW_Schneetarn\addons"^
     "-prefix=bw_snowCamo"^
     "-packonly"
